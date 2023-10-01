54784a799f6f91d64d87a2d83f570e53762f8a63
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-10-01 14:13:42 -0300

    Fiz passo 2.2 no JS

--- src/js/index.js
+++ src/js/index.js
@@ -9,8 +9,8 @@ Passo 1.3: Verificar se há algum botão selecionado e, se houver, remover a sel
 
 OBJETIVO 2: 
 Mostrar as informações do personagem quando clicar no botão respectivo.
-Passo 2.1: pegar ospersonagens no JS para poder mostrar ou esconder.
-Passo 2.2:
+Passo 2.1: Pegar os personagens no JS para poder mostrar ou esconder.
+Passo 2.2: Adicionar a classe .selecionado no personagem clicado.
 Passo 3.2:
  */
 
@@ -19,11 +19,13 @@ console.log(botoes);
 
 const personagens = document.querySelectorAll(".personagem"); //passo 2.1
 
-botoes.forEach(botao => {
+botoes.forEach((botao, indice) => {
     botao.addEventListener("click", () => {
         const botaoSelecionado = document.querySelector(".botao.selecionado"); //passo 1.3
         botaoSelecionado.classList.remove("selecionado"); //passo 1.3
         botao.classList.add("selecionado"); //passo 1.2
+
+        personagens[indice].classList.add("selecionado"); //passo 2.2
     });
 });
 
