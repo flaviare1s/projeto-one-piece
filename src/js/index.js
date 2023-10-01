3e942c51b6d00d027f5092d42ec438bb591dcf71
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-10-01 13:32:35 -0300

    Atualizei index.js fazendo o passo 1

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+/* OBJETIVO 1: Marcar o botão clicado como selecionado */
+/* Passo 1: pegar os botões no JS para poder verificar quando o usuário clicar nele */
+
+const botoes = document.querySelectorAll('.botao');
+console.log(botoes);
+
