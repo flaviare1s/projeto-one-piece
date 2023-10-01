83406bf5c9021be8a29d8cfe3828751a7f55dafd
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-10-01 14:18:00 -0300

    Terminei o último passo do JS e concluí o projeto

--- src/js/index.js
+++ src/js/index.js
@@ -11,7 +11,7 @@ OBJETIVO 2:
 Mostrar as informações do personagem quando clicar no botão respectivo.
 Passo 2.1: Pegar os personagens no JS para poder mostrar ou esconder.
 Passo 2.2: Adicionar a classe .selecionado no personagem clicado.
-Passo 3.2:
+Passo 3.2: Verificar se há alguma classe selecionada e, se houver, remover a seleção.
  */
 
 const botoes = document.querySelectorAll(".botao"); //passo 1.1
@@ -24,6 +24,9 @@ botoes.forEach((botao, indice) => {
         const botaoSelecionado = document.querySelector(".botao.selecionado"); //passo 1.3
         botaoSelecionado.classList.remove("selecionado"); //passo 1.3
         botao.classList.add("selecionado"); //passo 1.2
+
+        const personagemSelecionado = document.querySelector(".personagem.selecionado");
+        personagemSelecionado.classList.remove("selecionado"); //passo 3.3
 
         personagens[indice].classList.add("selecionado"); //passo 2.2
     });
